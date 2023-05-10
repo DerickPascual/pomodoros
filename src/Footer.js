580c09bd2710c4f6358abdc5dd41b23b0d0a6011
Derick <derickvanpascual@gmail.com>
2023-05-10 01:03:07 -0700

    Added background image and footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='Footer'>
+      <p className='Footer__p'>wallpaper by mikey123 at wallpapers.com</p>
+      <p className='Footer__p'>inspired by <a className='Footer__a' href="https://www.countdowns.live/">countdowns</a>, <a className="Footer__a" href="https://buildspace.so/home">buildspace</a> </p>
+    </div>
+  );
+}
+
+export default Footer;
